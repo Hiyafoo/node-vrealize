6524e48664c0877943983ad0cd08bd42c133f9c0
francisg3 <francisg3@hotmail.com>
2017-06-22 11:00:19 -0400

    Fix updateTemplateData tests

--- test/unit/specs/bin/lib/requests/updateTemplateData.spec.js
+++ test/unit/specs/bin/lib/requests/updateTemplateData.spec.js
@@ -71,122 +71,25 @@ describe('[Requests] - updateTemplateData method', function () {
     })
   })
 
+  it('promise should return unchanged templateData when dataToBeMerge is null', function () {
+    return vRa.updateTemplateData(templateData, null)
+    .then(function (template) {
+      expect(template).to.deep.equal(templateData)
+    })
+  })
+
+  it('promise should return unchanged templateData when dataToBeMerged has empty path', function () {
+    dataToBeMerged[0].path = ''
+    return vRa.updateTemplateData(templateData, dataToBeMerged)
+    .then(function (template) {
+      expect(template).to.equal(templateData)
+    })
+  })
+
   it('promise should return changed templateData with dataToBeMerged properties', function () {
     return vRa.updateTemplateData(templateData, dataToBeMerged)
     .then(function (template) {
       expect(template.data.DNS1.componentId).to.equal(dataToBeMerged[0].value)
     })
   })
-
-  // it('should not change templateData when no deploymentOptions are specified', function (done) {
-  //   var expectedResult = { test: 'test' }
-
-  //   requests.updateTemplateData(templateData, dataToBeMerged, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should modify the templatedData property at the root level', function (done) {
-  //   var expectedResult = { test: 'changed' }
-  //   var templateData = { test: 'test' }
-
-  //   requests.updateTemplateData(templateData, dataToBeMerged, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should modify the templatedData property at the nested path level', function (done) {
-  //   var deploymentOptions = {templateDataPath: 'dummy/path'}
-  //   var expectedResult = {
-  //     level1: {
-  //       level2: { test: 'changed' }
-  //     }
-  //   }
-  //   var templateData = {
-  //     level1: {
-  //       level2: { test: 'test' }
-  //     }
-  //   }
-
-  //   requests.updateTemplateData(templateData, deploymentOptions, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should modify the value of a templatedData property with a compounded name', function (done) {
-  //   var deploymentOptions = {templateDataPath: 'dummy/path'}
-  //   var expectedResult = {
-  //     level1: {
-  //       'level2.test': 'changed'
-  //     }
-  //   }
-  //   var templateData = {
-  //     level1: {
-  //       'level2.test': 'test'
-  //     }
-  //   }
-
-  //   requests.updateTemplateData(templateData, deploymentOptions, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should modify the value of templatedData properties with multiple values in the templateMap', function (done) {
-  //   var deploymentOptions = {templateDataPath: 'dummy/path'}
-  //   var expectedResult = {
-  //     leaf1: 'changed',
-  //     leaf2: 'changed'
-  //   }
-  //   var templateData = {
-  //     leaf1: {value: 'test'},
-  //     leaf2: {value: 'test'}
-  //   }
-
-  //   requests.updateTemplateData(templateData, deploymentOptions, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should modify the value of templatedData properties with multiple values in a real file', function (done) {
-  //   var deploymentOptions = {templateDataPath: path.resolve(__dirname, 'files/data.json')}
-  //   var expectedResult = {
-  //     leaf1: 'changed',
-  //     leaf2: 'changed'
-  //   }
-  //   var templateData = {
-  //     leaf1: {value: 'test'},
-  //     leaf2: {value: 'test'}
-  //   }
-  //   sandbox.restore()
-  //   requests.updateTemplateData(templateData, deploymentOptions, function (err, mergedTemplate) {
-  //     if (err) {}
-  //     expect(mergedTemplate).to.deep.equal(expectedResult)
-  //     done()
-  //   })
-  // })
-
-  // it('should throw an error when the file cannot be read', function (done) {
-  //   var deploymentOptions = {templateDataPath: path.resolve(__dirname, 'dummy/path')}
-  //   var templateData = {
-  //     leaf1: {value: 'test'},
-  //     leaf2: {value: 'test'}
-  //   }
-  //   sandbox.restore()
-  //   requests.updateTemplateData(templateData, deploymentOptions, function (err, mergedTemplate) {
-  //     if (err) {
-  //       expect(mergedTemplate).to.equal(null)
-  //       done()
-  //     }
-  //   })
-  // })
 })
